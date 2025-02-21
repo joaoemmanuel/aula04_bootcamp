2d37e4ae53937c7bd9b441a31fba24bb8dbfb90f
joaoemmanuel <joao.basass@gmail.com>
2025-02-21 11:21:49 -0300

    Incluí os comentários explicando os blocos do código

--- arquivos.py
+++ arquivos.py
@@ -1,14 +1,20 @@
 import csv
 
+# Setando o caminho a uma variável
 caminho_arq = "arquivos/exemplo.csv"
 
+# Inicializando a variável para receber os valores do csv
 dados = []
 
+# Abrindo o csv com o gerenciador de contexto "with"
 with open(file=caminho_arq, mode="r", encoding="utf-8") as arquivo:
     valores = csv.DictReader(arquivo)
+
+# Inserindo os valores dentro da lista dados
     for campos in valores:
        dados.append(campos)
 
+# Printando os valores contidos em dados
 for linhas in dados:
     print(linhas)
         
